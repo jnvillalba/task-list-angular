ad7ad92c2dc83e6e4cfb64523c5da01cee7ae2a6
jnvillalba <joaquinvillalba8@gmail.com>
2022-11-02 19:58:53 -0300

    mock de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "t1",
+        day: 'Noviembre 2 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: "t2",
+        day: 'Noviembre 2 a las 14:00',
+        reminder: true
+    },{
+        id:3,
+        text: "t3",
+        day: 'Noviembre 2 a las 20:00',
+        reminder: false
+    },
+]
