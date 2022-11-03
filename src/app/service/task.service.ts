da93c193639f8c3c8c9e626af3f678cafe4a5c37
jnvillalba <joaquinvillalba8@gmail.com>
2022-11-03 17:50:33 -0300

    servidor json

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
-import {Observable, of} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl= 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getTask(): Observable<Task[]>{
-    const tasks = of(TASKS)
-    return tasks
+    
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
